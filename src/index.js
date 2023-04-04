69a0b018d4aabbc48da958f1a0105f1df30304ab
Inna Nomerovkina <inkosof@gmail.com>
2023-04-04 17:07:12 +0300

    style

--- src/index.js
+++ src/index.js
@@ -32,27 +32,29 @@ function onSearchCountry(event) {
                 infoCountryEl.innerHTML = ``;
                 return;
             }
-            else
+            
             if (data.length >= 2 && data.length <= 10) {
                 
-                const countryList = data.map(({ name, flags }) => `<li class=""><img class="country-list__img" src="${flags.svg}" alt="${flags.alt}" width="20p" height="20px"/><p>${name.official}</p></li>`).join("");
+                const countryList = data.map(({ name, flags }) => `<li class="country-list__item"><img class="country-list__img" src="${flags.svg}" alt="${flags.alt}" width="40px" height="auto"/><p>${name.official}</p></li>`).join("");
                 listCoutryEl.innerHTML = countryList;
                 infoCountryEl.innerHTML = ``;
                 
                 return;
-            } else
+            } 
             if (data.length === 1) {
                        
-                const countryInfo = data.map(({ name, flags, languages, capital, population }) => `<img class="country-info__img" src="${flags.svg}" alt="${flags.alt}" width="40px" height="40px"/><p class="country-info__name">${name.official}</p><p class="country-info__capital"><span>Capital:</span>${capital}</p><p class="country-info__population"><span>Population:</span>${population}</p><p class="country-info__languages"><span>Languages:</span>${Object.values(languages)}</p>`).join("");
+                const countryInfo = data.map(({ name, flags, languages, capital, population }) => `<img class="country-info__img" src="${flags.svg}" alt="${flags.alt}" width="70px" height="auto"/><p class="country-info__name">${name.official}</p><p class="country-info__capital"><span class="country-info__span">Capital:</span > ${capital}</p><p class="country-info__population"><span class="country-info__span">Population:</span> ${population}</p><p class="country-info__languages"><span class="country-info__span">Languages: </span>${Object.values(languages)}</p>`).join("");
                 
                 infoCountryEl.innerHTML = countryInfo;
-                  listCoutryEl.innerHTML = ``;
+                listCoutryEl.innerHTML = ``;
                 return;
             }
             
         })
         .catch(error => {
             console.log(error.message);
+            listCoutryEl.innerHTML = ``;
+            infoCountryEl.innerHTML = ``;
         });
 }
 
